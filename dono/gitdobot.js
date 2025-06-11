e568398623117123ae21f5a49ac5d47723a68c83
Rafa23xz <pedro.mds453@gmail.com>
2025-06-12 01:34:43 +0200

    Update gitdobot.js

--- dono/gitdobot.js
+++ dono/gitdobot.js
@@ -2,7 +2,7 @@ const gitdobot = (prefix) => {
 return`
 Para saber como instalar o termux e da os comandos, veja esse site:
 
-https://aleatory.vip/instalacao
+https://github.com/Rafa23xz/Hinata-MD
 
 
 `;
